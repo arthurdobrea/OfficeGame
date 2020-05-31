532c01044e18864a899e9b0bf8479c053ae46d1e
arthurdobrea <arthurdobrya@gmail.com>
2020-05-31 15:26:16 +0300

    Add start and end transition and menu

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     private AudioManager audioManager;
+    public Animator transition;
+    public float transitionTime = 1f;
 
     private void Start()
     {
@@ -14,13 +17,20 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-    
-    
 }
